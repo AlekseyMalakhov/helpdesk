e393f00aa7e6045a96c5dc06583442b1cbff6fe0
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2026-04-17 16:52:19 +0300

    add  auth

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use(express.json());
 
 app.get("/api/health", (_req, res) => {
